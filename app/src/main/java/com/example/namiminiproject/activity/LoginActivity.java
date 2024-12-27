cc7ba021a5b8719c017c0e902854c1f7e2926b20
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-12-27 22:03:08 +0545

    register and login logic with sharedPref

--- app/src/main/java/com/example/namiminiproject/activity/LoginActivity.java
+++ app/src/main/java/com/example/namiminiproject/activity/LoginActivity.java
@@ -3,45 +3,112 @@ package com.example.namiminiproject.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import com.example.namiminiproject.AppDatabase;
 import com.example.namiminiproject.R;
+import com.example.namiminiproject.sharedPref.SharedPrefManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     // Views
-    Button loginButton;
-    EditText userName;
-    EditText userPassword;
+    AppCompatButton loginButton;
+    EditText loginUserName, loginUserPassword;
+
+    TextView signUp;
 
     // Variables
+    String name, password;
+    Intent intent;
+    AppDatabase appDatabase;
+
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        appDatabase = AppDatabase.getDatabase(LoginActivity.this);
+        sharedPrefManager = SharedPrefManager.getInstance(LoginActivity.this);
+
         initView();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, TodoActivity.class);
-                startActivity(intent);
-                finish();
+
+                initVariable();
+
+                if(name.isEmpty()) {
+                    loginUserName.setError("Username shouldn't be empty !!");
+                } else if (password.isEmpty()) {
+                    loginUserPassword.setError("Password shouldn't be empty !!");
+                } else {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int userExist = appDatabase.userDao().isUserNameExist(name);
+
+                            if (userExist > 0) {
+                                int passwordExist = appDatabase.userDao().isUserPasswordExist(name, password);
+                                if (passwordExist > 0) {
+
+                                    sharedPrefManager.saveLoggedIn(true);
+
+                                    navigateTo(TodoListItemActivity.class);
+
+                                } else {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(LoginActivity.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(LoginActivity.this, "Invalid username", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    }).start();
+                }
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateTo(RegisterActivity.class);
             }
         });
 
     }
 
     void initView(){
-        loginButton = (Button) findViewById(R.id.login_button);
-        userName = (EditText) findViewById(R.id.user_name);
-        userPassword = (EditText) findViewById(R.id.user_password);
+        loginButton = (AppCompatButton) findViewById(R.id.login_button);
+        loginUserName = (EditText) findViewById(R.id.et_login_userName);
+        loginUserPassword = (EditText) findViewById(R.id.et_login_userPassword);
+        signUp = (TextView) findViewById(R.id.tv_signUp);
+    }
+
+    void initVariable(){
+        name = loginUserName.getText().toString();
+        password = loginUserPassword.getText().toString();
+    }
+    void navigateTo(Class activityClass) {
+        intent = new Intent(LoginActivity.this,activityClass);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/namiminiproject/activity/RegisterActivity.java
+++ app/src/main/java/com/example/namiminiproject/activity/RegisterActivity.java
@@ -2,23 +2,122 @@ package com.example.namiminiproject.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import com.example.namiminiproject.AppDatabase;
 import com.example.namiminiproject.R;
+import com.example.namiminiproject.entities.User;
+import com.example.namiminiproject.sharedPref.SharedPrefManager;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    // Views
+    AppCompatButton registerButton;
+    EditText registerUsername, registerUserEmail, registerUserPassword, registerUserConfirmPassword;
+    TextView signIn;
+
+    // Variables
+    AppDatabase appDatabase;
+    String name, email, password, confirmPassword;
+    SharedPrefManager sharedPrefManager;
+    Intent intent;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        new Handler().postDelayed(()->{
-            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }, 2000);
+        appDatabase = AppDatabase.getDatabase(RegisterActivity.this);
+        sharedPrefManager = SharedPrefManager.getInstance(RegisterActivity.this);
+
+        initView();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                initVariable();
+
+                if (name.isEmpty()) {
+
+                    registerUsername.setError("Name shouldn't be empty !!");
+
+                } else if (email.isEmpty()) {
+
+                    registerUserEmail.setError("Email shouldn't be empty !!");
+
+                } else if (password.isEmpty()) {
+
+                    registerUserPassword.setError("Password shouldn't be empty !!");
+
+                } else if (confirmPassword.isEmpty()) {
+
+                    registerUserConfirmPassword.setError("Confirm password shouldn't  be empty !!");
+
+                } else {
+                    new Thread(()->{
+                        User insertUser = new User(name, email, password);
+                        int count = appDatabase.userDao().isUserExist(email, name);
+                        if (count > 0) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(RegisterActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            appDatabase.userDao().insertUser(insertUser);
+
+                            sharedPrefManager.saveLoggedIn(true);
+
+                            navigateTo(TodoListItemActivity.class);
+
+                            System.out.println("User added to db !!");
+                        }
+
+                    }).start();
+
+                }
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateTo(LoginActivity.class);
+            }
+        });
+
+    }
+
+    void initView(){
+        registerButton = (AppCompatButton) findViewById(R.id.register_button);
+        registerUsername = (EditText) findViewById(R.id.et_register_userName);
+        registerUserEmail = (EditText) findViewById(R.id.et_register_userEmail);
+        registerUserPassword = (EditText) findViewById(R.id.et_register_userPassword);
+        registerUserConfirmPassword = (EditText) findViewById(R.id.et_register_userConfirmPassword);
+        signIn = (TextView) findViewById(R.id.tv_signIn);
+    }
+
+    void initVariable() {
+        name = registerUsername.getText().toString();
+        email = registerUserEmail.getText().toString();
+        password = registerUserPassword.getText().toString();
+        confirmPassword = registerUserConfirmPassword.getText().toString();
+    }
+
+
+   void navigateTo(Class activityClass) {
+        intent = new Intent(RegisterActivity.this,activityClass);
+        startActivity(intent);
+        finish();
     }
 }
