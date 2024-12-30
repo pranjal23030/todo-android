81a4197cf9331aa61d00930a46bed65b3e197466
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-12-30 21:03:13 +0545

    layout to activity

--- app/src/main/java/com/example/namiminiproject/activity/CreateTaskActivity.java
+++ app/src/main/java/com/example/namiminiproject/activity/CreateTaskActivity.java
@@ -7,10 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.namiminiproject.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class CreateTaskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_create_task);
     }
 }
